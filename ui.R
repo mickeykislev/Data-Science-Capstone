ad7021b7955d69af1aeedeb308ba55c1482b3e6f
Mickey Kislev <32208712+mickeykislev@users.noreply.github.com>
2021-08-25 01:48:16 +0300

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,9 +1,10 @@
 # Load packages
 library(shiny)
 library(shinybusy)
+library(shinythemes)
 
-shinyUI(fluidPage(
-    titlePanel("Predict the word"),
+shinyUI(fluidPage(theme = shinytheme("darkly"),
+    titlePanel("Predict the next word"),
     sidebarPanel(
         textInput("caption", "Write a word")),
     mainPanel(
@@ -14,11 +15,9 @@ shinyUI(fluidPage(
                     tabPanel("twitter", tableOutput("value3")),
                     tabPanel("news", tableOutput("value4"))
        ),
- p("This app was designed as the final project for the course Data Science Capstone, as part of the Data Science Specialization, by Johns Hopkins University"),
   br(), 
-p("The project was developed by Mickey Kislev, 25.8.2021"),
-HTML("<p>github repository with all the files <a href='https://github.com/mickeykislev/Data-Science-Capstone'> https://github.com/mickeykislev/Data-Science-Capstone</a></p>")
-
+p("This app was designed as the final project for the course Data Science Capstone, as part of the Data Science Specialization, by Johns Hopkins University", style = "font-size:10px"),
+p("The project was developed by Mickey Kislev, 25.8.2021", style = "font-size:10px"),
        )
     )
     )
